fec423868354766a45cc67d95e33137a17963f03
moz <moz@local>
2016-01-08 22:50:17 +0100

    added csv write example

---
+++ csvwrite_example.py
@@ -0,0 +1,51 @@
+import csv
+
+outputfile = "csvwrite.csv"
+
+data = [["web01", 101, "SRV"],
+		["web02", 102, "SRV"],
+		["web03", 103, "SRV"]]
+
+print "Output data is"
+print "   ", data
+print ""
+		
+print "writing data to file: %s"%outputfile
+print "To view:"
+print "   cat %s"%outputfile
+with open(outputfile, 'wb') as csvfile:
+	writer = csv.writer(csvfile, delimiter='\t')
+	for row in data:
+		writer.writerow(row)
+print ""
+
+
+#
+# output of csv writer using headers
+#
+outputfile_dict = "csvwrite_dict.csv"
+
+data = [{ "name": "web01", "id": 101, "LAN": "SRV"}, 
+		{ "name": "web02", "id": 102, "LAN": "SRV"},
+		{ "name": "web03", "id": 103, "LAN": "SRV"}]
+
+print "Output data is"
+print "   ", data
+
+fieldnames = data[0].keys()
+print "Fields are ", fieldnames
+print ""
+	
+print "writing dictionary data to file: %s"%outputfile_dict
+print "To view:"
+print "   cat %s"%outputfile_dict
+
+with open(outputfile_dict, 'wb') as csvfile:
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
+	writer.writeheader()
+
+	for row in data:
+		writer.writerow( row )
+print ""
+
+
